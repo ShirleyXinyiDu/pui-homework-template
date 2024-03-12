01e3200a7ffbc1bbd3d1a2b7782f439c52cf4ba5
ShirleyXinyiDu <157089144+ShirleyXinyiDu@users.noreply.github.com>
2024-03-12 12:49:44 -0400

    correct

--- solution-hw5/README.md
+++ solution-hw5/README.md
@@ -4,4 +4,4 @@ Deployed website: https://shirleyxinyidu.github.io/pui-homework-template/
 
 Repository link: https://github.com/ShirleyXinyiDu/pui-homework-template.git
 
-Link to project gallery page: https://shirleyxinyidu.github.io/pui-homework-template/solution-hw6/
+Link to project gallery page: https://shirleyxinyidu.github.io/pui-homework-template/solution-hw5/
--- solution-hw6/js/updateBadge.js
+++ solution-hw6/js/updateBadge.js
@@ -5,7 +5,9 @@ function updateBadge() {
     const cartCount = document.querySelector('p.cart-badge'); //select the cart rolls counter element
     cartArrayString = localStorage.getItem('storedCartRolls'); //get storedCartRolls from local storage
     cartArrayObject = JSON.parse(cartArrayString); //JSON --> object
-    cartCount.innerText = cartArrayObject.length; //update the cart rolls numbers
+    if (cartArrayObject.length != null) {
+        cartCount.innerText = cartArrayObject.length; //update the cart rolls numbers when there are rolls 
+    }
 }
 
 updateBadge();
