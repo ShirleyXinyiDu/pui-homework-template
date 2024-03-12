ded5a15809afe7542ebd7569c9608a6c6cf15eb8
ShirleyXinyiDu <157089144+ShirleyXinyiDu@users.noreply.github.com>
2024-03-12 12:46:43 -0400

    add line to hw6

--- solution-hw6 copy/js/updateBadge.js
+++
@@ -1,8 +0,0 @@
-function updateBadge() {
-    const cartCount = document.querySelector('p.cart-badge');
-    console.log(cartCount);
-    console.log(cartArray.length);
-    cartCount.innerText = cartArray.length;
-}
-
-updateBadge();
