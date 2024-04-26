0d20fad0aa39aae29d23d563943bd8559fb0daa1
ShirleyXinyiDu <xinyidu@andrew.cmu.edu>
2024-04-26 17:19:35 -0400

    111

--- final-project/js/posters.js
+++ final-project/js/posters.js
@@ -1,61 +1,41 @@
-// const sortingIcon = document.getElementById('sorting-icon');
-
+// posters.js
 
 const folder = "assets/posters";
-const images = [];
+const images = []; 
 
-// Using fetch to load the content of the folder
-fetch(folder)
-  .then(response => response.text())
-  .then(data => {
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(data, "text/html");
+$.ajax({
+    url: folder,
+    success: function (data) {
+        // var images = [];
 
-    // Collect all image URLs
-    const links = htmlDocument.getElementsByTagName("a");
-    for (let i = 0; i < links.length; i++) {
-      let val = links[i].getAttribute("href");
-      if (val.match(/\.(jpg)$/)) {  // Checking if the link is for a .jpg image
-        // console.log('val');
-        // console.log(val);
-        images.push(val);
-        // images.push(folder + val);  // Appending the full path to the images array
-      }
+        // Collect all image URLs
+        $(data).find("a").attr("href", function (i, val) {
+            if (val.match(/\.(jpg)$/)) {
+                console.log('val');
+                console.log(val);
+                images.push(val);
+            }
+        });
+
+        sortImagesByTitle(images);
+        console.log(images);
+
+        images.forEach(function(val) {
+            var detailPageUrl = "./poster-detail.html?imageUrl=" + encodeURIComponent(val);
+        
+            var imgElement = $("<img>").attr("src", val);
+            var linkElement = $("<a>").attr("href", detailPageUrl).append(imgElement);
+        
+            $("#poster-gallery").append(linkElement);
+        });
+
+
     }
-
-    // images.sort();  // Sorting images array; modify sorting logic as needed
-
-
-    // if (sortingIcon.src.includes("ranking.svg")) {
-    //   sortImagesByRanking(images);
-    //   console.log('sortImagesByRanking');
-    //   console.log(images);
-    // } else if (sortingIcon.src.includes("title.svg")) {
-    //   console.log('sortImagesByTitle');
-    //   console.log(images);
-    // } 
-
-    sortImagesByRanking(images);
-    console.log(images);
-
-    images.forEach(function(val) {
-      const detailPageUrl = "./poster-detail.html?imageUrl=" + encodeURIComponent(val);
-
-      const imgElement = document.createElement("img");
-      imgElement.src = val;
-
-      const linkElement = document.createElement("a");
-      linkElement.href = detailPageUrl;
-      linkElement.appendChild(imgElement);
-
-      document.querySelector("#poster-gallery").appendChild(linkElement);
-    });
-  })
-  .catch(error => console.error("Failed to load folder content", error));
+});
 
 
 
-function sortImagesByRanking(filenames) {
+function sortImagesByTitle(filenames) {
     return filenames.sort(function(a, b) {
         // Extract the numerical part of the filename after the last '/' and decode it
         let numberA = parseInt(decodeURIComponent(a).match(/(\d+)/)[0], 10);
@@ -65,7 +45,4 @@ function sortImagesByRanking(filenames) {
         return numberA - numberB;
     });
 }
-
-
-
 
---
+++ final-project/js/posters1.js
@@ -0,0 +1,60 @@
+// const sortingIcon = document.getElementById('sorting-icon');
+
+
+const folder = "assets/posters";
+const images = [];
+
+// Using fetch to load the content of the folder
+fetch(folder)
+  .then(response => response.text())
+  .then(data => {
+    const parser = new DOMParser();
+    const htmlDocument = parser.parseFromString(data, "text/html");
+
+    // Collect all image URLs
+    const links = htmlDocument.getElementsByTagName("a");
+    for (let i = 0; i < links.length; i++) {
+      let val = links[i].getAttribute("href");
+      if (val.match(/\.(jpg)$/)) {  // Checking if the link is for a .jpg image
+        // console.log('val');
+        console.log(val);
+        images.push(val);
+        // images.push(folder + val);  // Appending the full path to the images array
+      }
+    }
+
+
+    sortImagesByRanking(images);
+    console.log(images);
+
+    images.forEach(function(val) {
+      const detailPageUrl = "./poster-detail.html?imageUrl=" + encodeURIComponent(val);
+
+      const imgElement = document.createElement("img");
+      imgElement.src = val;
+
+      const linkElement = document.createElement("a");
+      linkElement.href = detailPageUrl;
+      linkElement.appendChild(imgElement);
+
+      document.querySelector("#poster-gallery").appendChild(linkElement);
+    });
+  })
+  .catch(error => console.error("Failed to load folder content", error));
+
+
+
+function sortImagesByRanking(filenames) {
+    return filenames.sort(function(a, b) {
+        // Extract the numerical part of the filename after the last '/' and decode it
+        let numberA = parseInt(decodeURIComponent(a).match(/(\d+)/)[0], 10);
+        let numberB = parseInt(decodeURIComponent(b).match(/(\d+)/)[0], 10);
+
+        // Compare the numbers to determine the order
+        return numberA - numberB;
+    });
+}
+
+
+
+
