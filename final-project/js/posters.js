1a6b31d78d832c31b7c7f1f43331e46449857170
ShirleyXinyiDu <xinyidu@andrew.cmu.edu>
2024-04-26 17:56:26 -0400

    11

--- final-project/js/posters.js
+++ final-project/js/posters.js
@@ -1,68 +1,31 @@
-// const sortingIcon = document.getElementById('sorting-icon');
-
-
-// const folder = "assets/posters";
-
-const folder = "assets/icons";
-const images = [];
-
-// Using fetch to load the content of the folder
-fetch(folder)
-  .then(response => response.text())
-  .then(data => {
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(data, "text/html");
-
-    // Collect all image URLs
-    const links = htmlDocument.getElementsByTagName("a");
-    for (let i = 0; i < links.length; i++) {
-      let val = links[i].getAttribute("href");
-      if (val.match(/\.(jpg)$/)) {  // Checking if the link is for a .jpg image
-        // console.log('val');
-        console.log(val);
-        images.push(val);
-        // images.push(folder + val);  // Appending the full path to the images array
+// Function to create and append image elements to the gallery
+function createAndAppendPosterElements(movies) {
+    const gallery = document.querySelector("#poster-gallery");
+  
+    // Loop through each movie in the movies object
+    for (const imageName in movies) {
+      if (movies.hasOwnProperty(imageName)) {
+        const val = imageName; // The image name is the key in the movies object
+        const detailPageUrl = "./poster-detail.html?imageUrl=" + encodeURIComponent(val);
+  
+        // Create img element for poster
+        const imgElement = document.createElement("img");
+        imgElement.src = "./assets/posters/" + val;
+        imgElement.alt = movies[imageName].title; // Optional: add alt text for accessibility
+  
+        // Create anchor element wrapping the image
+        const linkElement = document.createElement("a");
+        linkElement.href = detailPageUrl;
+        linkElement.appendChild(imgElement);
+  
+        // Append the anchor element to the gallery
+        gallery.appendChild(linkElement);
       }
     }
-
-
-    sortImagesByRanking(images);
-    console.log(images);
-
-    images.forEach(function(val) {
-      const detailPageUrl = "./poster-detail.html?imageUrl=" + encodeURIComponent(val);
-
-      const imgElement = document.createElement("img");
-      imgElement.src = val;
-
-      const linkElement = document.createElement("a");
-      linkElement.href = detailPageUrl;
-      linkElement.appendChild(imgElement);
-
-      document.querySelector("#poster-gallery").appendChild(linkElement);
-    });
-  })
-  .catch(error => console.error("Failed to load folder content", error));
-
-
-  const imgElement1 = document.createElement("img");
-  imgElement1.src = "./assets/posters/1%20jeanne%20dielman.jpg";
-  document.querySelector("#poster-gallery").appendChild(imgElement1);
-
-
-  // $("#poster-gallery").append($("<img>").attr("src", "./assets/posters/1%20jeanne%20dielman.jpg"));
-
-function sortImagesByRanking(filenames) {
-    return filenames.sort(function(a, b) {
-        // Extract the numerical part of the filename after the last '/' and decode it
-        let numberA = parseInt(decodeURIComponent(a).match(/(\d+)/)[0], 10);
-        let numberB = parseInt(decodeURIComponent(b).match(/(\d+)/)[0], 10);
-
-        // Compare the numbers to determine the order
-        return numberA - numberB;
-    });
-}
-
-
-
-
+  }
+  
+  // Assuming the movies data is structured as provided in the `movieData.js` file,
+  // and that `movieData.js` is correctly linked before this script in your HTML,
+  // you can call the function to populate the gallery.
+  createAndAppendPosterElements(movies);
+  
