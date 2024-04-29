b9874be693c9b7d6775d41479bc49ecd7b1f4f75
ShirleyXinyiDu <xinyidu@andrew.cmu.edu>
2024-04-29 02:42:50 -0400

    11

---
+++ final-project/js/animate.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+  $('#web-name').textillate({
+      // Set the type of animation
+      in: {
+          effect: 'fadeInLeft', // Choose the animation effect
+          delayScale: 1.5,      // Timing of the delay between letters
+          delay: 50             // Delay between each letter animation
+      },
+      loop: true // Set to true if you want it to loop, false otherwise
+  });
+});
+
+
+$(document).ready(function () {
+  $('.animate-paragraph1').textillate({
+      // Set the type of animation
+      in: {
+          effect: 'fadeInLeft', // Choose the animation effect
+          delayScale: 1.5,      // Timing of the delay between letters
+          delay: 5             // Delay between each letter animation
+      },
+      loop: true // Set to true if you want it to loop, false otherwise
+  });
+});
+
+$(document).ready(function () {
+  $('.animate-paragraph2').textillate({
+      // Set the type of animation
+      in: {
+          effect: 'fadeInUp', // Choose the animation effect
+          delayScale: 1.5,      // Timing of the delay between letters
+          delay: 10             // Delay between each letter animation
+      },
+      loop: true // Set to true if you want it to loop, false otherwise
+  });
+});
+
+$(document).ready(function () {
+  $('.animate-paragraph3').textillate({
+      // Set the type of animation
+      in: {
+          effect: 'fadeInUp', // Choose the animation effect
+          delayScale: 1.5,      // Timing of the delay between letters
+          delay: 20             // Delay between each letter animation
+      },
+      loop: true // Set to true if you want it to loop, false otherwise
+  });
+});
+
+$(document).ready(function () {
+  $('.animate-paragraph4').textillate({
+      // Set the type of animation
+      in: {
+          effect: 'fadeInLeft', // Choose the animation effect
+          delayScale: 1.5,      // Timing of the delay between letters
+          delay: 30             // Delay between each letter animation
+      },
+      loop: true // Set to true if you want it to loop, false otherwise
+  });
+});
+
+
+$(document).ready(function () {
+  $('#movie-title').textillate({
+      // Set the type of animation
+      in: {
+          effect: 'fadeInLeft', // Choose the animation effect
+          delayScale: 1.5,      // Timing of the delay between letters
+          delay: 30             // Delay between each letter animation
+      },
+      loop: true // Set to true if you want it to loop, false otherwise
+  });
+});
+
+$(document).ready(function () {
+  $('.animate-paragraph').textillate({
+      autoStart: true,
+      in: {
+          effect: 'fadeInLeft', // You can change the effect as needed
+          delayScale: 1.5,
+          delay: 20
+      }
+  });
+});
+
+
+
+
+
